6b6c497dbf1f4440d0f74de7195babc271b15055
Aabhas Jain <aabhasmnit@gmail.com>
2017-12-27 10:50:26 +0530

    First Commit

---
+++ app/src/main/java/com/express/todoandroidapp/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.express.todoandroidapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by root on 27/12/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
